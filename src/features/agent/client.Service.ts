7e952bd1181996b1742a791eba0baa64fcc5aab6
JAVIER CORDOVA <xavier170791@gmail.com>
2026-02-12 00:07:01 -0500

    fix #14 agendado

--- src/features/agent/client.Service.ts
+++ src/features/agent/client.Service.ts
@@ -11,28 +11,17 @@ export const agentService = {
         const requestOptions    =   {
             method: "GET",
             headers: {
-                'Accep': 'application/json',
+                'Accept': 'application/json',
                 'Authorization': `Bearer ${token}`
             }
         }
-        //const res = await fetch("http://localhost:8000/crm/getAgent", requestOptions)
-        //if(res.status == 401){
-        //        localStorage.removeItem("token")
-        //        window.location.href    =   "/login?message=expired";
-        //        return;
-        //}
-        const res = {
-                    status: 200,
-                    ok: true,
-                    json: async () => ({
-                        status: 200,
-                        data: [
-                            { date: '2026-02-01', descripcion: 'holamundo', cliente: 'AB' },
-                            { date: '2026-02-02', descripcion: 'segunda prueba', cliente: 'CD' }
-                        ]
-                    })
-                }
-        
+        const res = await fetch("http://localhost:8000/crm/getCalendar", requestOptions)
+        if(res.status == 401){
+                localStorage.removeItem("token")
+                window.location.href    =   "/login?message=expired";
+                return;
+        }
+       
         if(!res.ok){
                 throw new Error(" Problemas obtener Client");
                 }
